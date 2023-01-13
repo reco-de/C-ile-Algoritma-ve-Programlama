6c2a384f553f03757479cdbe489f0d56af07b61c
Recep Yıldırım <95899401+reco-de@users.noreply.github.com>
2023-01-13 12:37:27 +0300

    Add files via upload

---
+++ Turkcell_Proje_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Turkcell_Proje_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Kimlik****");
+            Console.WriteLine();
+            Console.WriteLine("Recep");
+            Console.WriteLine("Yıldırım");
+            Console.WriteLine("Akü");
+            Console.Write("Denizli");
+            Console.WriteLine();
+            Console.WriteLine("****Kimlik****");
+        }
+    }
+}
